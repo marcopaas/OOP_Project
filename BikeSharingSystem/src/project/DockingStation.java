8071fb0c70149ebb51c303f72b91e3f76544c534
layaljabbouri <102412669+layaljabbouri@users.noreply.github.com>
2023-04-24 18:38:33 +0200

    Add files via upload

---
+++ BikeSharingSystem/src/project/DockingStation.java
@@ -0,0 +1,21 @@
+package project;
+
+public class DockingStation {
+	
+	private int numberReturns;
+	private int numberRents;
+	public int getNumberReturns() {
+		return numberReturns;
+	}
+	public void setNumberReturns(int numberReturns) {
+		this.numberReturns = numberReturns;
+	}
+	public int getNumberRents() {
+		return numberRents;
+	}
+	public void setNumberRents(int numberRents) {
+		this.numberRents = numberRents;
+	}
+	
+	
+}
---
+++ BikeSharingSystem/src/project/Location.java
@@ -0,0 +1,7 @@
+package project;
+
+public class Location {
+	
+	 public double distanceTo(Location that) {return 0;
+	 }
+}
